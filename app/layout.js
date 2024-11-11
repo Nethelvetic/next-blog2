730853d8f9deaedece42d5a00b77db8a1881ddd6
Golliard <golliard.info@gmail.com>
2024-11-11 13:51:46 +0100

    correction

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,11 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-  title: 'Mon Blog',
-  description: 'Un blog Next.js',
-};
+import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">.
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-export default {
-    output: 'export',
-    trailingSlash: true,
-    assetPrefix: '/next-blog2', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub
-  };
-  
+const nextConfig = {};
+
+export default nextConfig;
