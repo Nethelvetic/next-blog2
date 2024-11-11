56895f12dd7142adac860d9cbe4a7adc11a2decd
Golliard <golliard.info@gmail.com>
2024-11-11 13:41:21 +0100

    correctif github

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 export default {
     output: 'export',
     trailingSlash: true,
-    assetPrefix: './',
+    assetPrefix: '/next-blog2', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub
   };
   
